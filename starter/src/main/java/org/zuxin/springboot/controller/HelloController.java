08da3a1f4f70d54c6522d5abfc1a2e3aded16cc0
旺新 <zuxin.lzx@alibaba-inc.com>
2017-12-26 17:37:10 +0800

    支持属性配置

--- starter/src/main/java/org/zuxin/springboot/controller/HelloController.java
+++ starter/src/main/java/org/zuxin/springboot/controller/HelloController.java
@@ -1,11 +1,15 @@
 package org.zuxin.springboot.controller;
 
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -15,8 +19,14 @@ import java.util.Map;
  * Time:   下午4:20
  */
 @RestController
+@ConfigurationProperties(prefix="my")
 @RequestMapping(value = "/hello")
+@Data
 public class HelloController {
+    private String name;
+    private Integer port;
+    private List<String> servers = new ArrayList<String>();
+
     private Map<String, Object> success(Object data) {
         Map<String, Object> result = new HashMap<String, Object>();
         result.put("success", true);
@@ -29,5 +39,10 @@ public class HelloController {
     @GetMapping(value = "/say")
     public Object say(@RequestParam String msg) {
         return success(msg);
+    }
+
+    @GetMapping(value = "/getConfig")
+    public Object getConfig() {
+        return success(name + port + servers);
     }
 }
